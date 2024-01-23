230c18e26d1731f92a226ce895f25298f457e0bc
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2024-01-23 17:48:24 +0100

    Create template cache on application start

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"log/slog"
 	"net/http"
 	"os"
@@ -9,22 +10,30 @@ import (
 var version string
 
 type application struct {
-	logger  *slog.Logger
-	version string
+	logger        *slog.Logger
+	templateCache map[string]*template.Template
+	version       string
 }
 
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
 	app := &application{
-		logger:  logger,
-		version: version,
+		logger:        logger,
+		templateCache: templateCache,
+		version:       version,
 	}
 
 	port := ":8998"
 	logger.Info("starting server", "port", port)
 
-	err := http.ListenAndServe(port, app.routes())
+	err = http.ListenAndServe(port, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
