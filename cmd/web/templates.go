e65584411973be16af525c4a9430c9a2ab936162
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2024-01-23 17:46:34 +0100

    Write method to cache templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"html/template"
+	"path/filepath"
+)
+
 type templateData struct {
 	Version string
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+	for _, page := range pages {
+		name := filepath.Base(page)
+		files := []string{
+			"./ui/html/base.tmpl",
+			page,
+		}
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+		cache[name] = ts
+	}
+	return cache, nil
 }
