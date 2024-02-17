8e6a1ace64e198ec33f0f9c1bd978982b919e0b7
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2024-02-17 11:08:13 +0100

    Add method for swim summarize

--- internal/models/swims.go
+++ internal/models/swims.go
@@ -13,8 +13,15 @@ type Swim struct {
 	Assessment int
 }
 
+type Summary struct {
+	TotalDistance int
+	Count         int
+}
+
 type SwimModel interface {
 	Get() (*Swim, error)
+	GetAll() ([]*Swim, error)
+	Summarize([]*Swim) *Summary
 }
 
 type swimModel struct {
@@ -42,4 +49,44 @@ func (sw *swimModel) Get() (*Swim, error) {
 	}
 
 	return &s, nil
+}
+
+func (sw *swimModel) GetAll() ([]*Swim, error) {
+	stmt := `SELECT date, distance_m, assessment FROM tracks_track WHERE user_id = $1 ORDER BY date ASC;`
+
+	rows, err := sw.DB.Query(stmt, 1)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var swims []*Swim
+	for rows.Next() {
+		var s Swim
+		errScan := rows.Scan(&s.Date, &s.DistanceM, &s.Assessment)
+		if errScan != nil {
+			return nil, errScan
+		}
+
+		swims = append(swims, &s)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return swims, nil
+}
+
+func (sw *swimModel) Summarize(swims []*Swim) *Summary {
+	var totalDistance int
+	var count int
+
+	for _, swim := range swims {
+		totalDistance += swim.DistanceM
+		count++
+	}
+
+	return &Summary{TotalDistance: totalDistance, Count: count}
 }
